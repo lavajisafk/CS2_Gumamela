ce4188b3e6bfdfc5dc52a55b5f553d34700d641b
lavajisafk <jlava2031@wvc.pshs.edu.ph>
2026-09-02 06:08:49 +0000

    Update Lava_Joseph_DistanceProgram.py.py

--- Lava_Joseph_DistanceProgram.py.py
+++ Lava_Joseph_DistanceProgram.py.py
@@ -1,18 +1,18 @@
 import math
 
+#Ask the user to enter the coordinates of the first point and second point
 point_x1 = float(input("enter the x1:"))
 point_x2 = float(input("enter the x2:"))
 point_y1 = float(input("enter the y1:"))
 point_y2 = float(input("enter the y2:"))
 
-#distance = sqrt(pow(point_x2-point_x1, 2) + pow(point_y2-point_1, 2)
-
+#Compute the distance using the distance formula
 point_a = pow(point_x2 - point_x1, 2)
 point_b = pow(point_y2 - point_y1, 2)
 result = point_a + point_b
 distance = math.sqrt(point_a + point_b)
 
+#Display the results
 print("the distance is", distance)
 
-#area_of_circle = (math.pi * math.pow(radius, 2))
-#print("\nThe area of circle", area_of_circle)
+
